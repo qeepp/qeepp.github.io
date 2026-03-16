1114068f7efdf70059d853daea7f48864f674228
Rado Dimitrov <contact@qeepp.com>
2026-03-16 14:49:18 -0400

    Update

--- assets/js/generate-sitemap.js
+++ assets/js/generate-sitemap.js
@@ -77,7 +77,7 @@ function buildSitemap(urlEntries) {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${body}
-</urlset>
+</urlset> 
 `;
 }
 
